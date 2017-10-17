bae55a378feee4f303aa0177328ac40f6600816a
Test User <testuser@Shyamals-iMac.local>
2017-10-17 05:04:40 -0500

    Added the framework folder

---
+++ ScienceKit/ScienceKit/ScienceKit.h
@@ -0,0 +1,19 @@
+//
+//  ScienceKit.h
+//  ScienceKit
+//
+//  Created by Test User on 10/17/17.
+//  Copyright © 2017 Shyamal Chandra. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ScienceKit.
+FOUNDATION_EXPORT double ScienceKitVersionNumber;
+
+//! Project version string for ScienceKit.
+FOUNDATION_EXPORT const unsigned char ScienceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScienceKit/PublicHeader.h>
+
+
