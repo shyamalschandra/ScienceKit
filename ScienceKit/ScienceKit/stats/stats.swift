7541c09a6084462164bf3ace59dacc8f2dcbf3bd
Test User <testuser@Shyamals-iMac.local>
2017-10-17 05:32:50 -0500

    Added the stats package

---
+++ ScienceKit/ScienceKit/stats/stats.swift
@@ -0,0 +1,22 @@
+//
+//  stats.swift
+//  ScienceKit
+//
+//  Created by Shyamal Chandra on 10/17/17.
+//  Copyright © 2017 Shyamal Chandra. All rights reserved.
+//
+// This software is provided "as-is".  There are no expressed or implied
+// warranties of any kind, including, but not limited to, the warranties
+// of merchantability and fitness for a given application.  In no event
+// shall Gary Strangman be liable for any direct, indirect, incidental,
+// special, exemplary or consequential damages (including, but not limited
+// to, loss of use, data or profits, or business interruption) however
+// caused and on any theory of liability, whether in contract, strict
+// liability or tort (including negligence or otherwise) arising in any way
+// out of the use of this software, even if advised of the possibility of
+// such damage.
+
+import Foundation
+import Accelerate
+
+
